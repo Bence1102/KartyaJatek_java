d371d018ee8ec24ff8977eac9038661e4cadb90a
Bottesz <botyaring@gmail.com>
2025-09-10 14:46:50 +0200

    osszeallit,kirak

--- src/main/KartyaJatek.java
+++ src/main/KartyaJatek.java
@@ -1,20 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package main;
 
-/**
- *
- * @author PolgárBotond(SZF_202
- */
-public class KartyaJatek {
 
-    /**
-     * @param args the command line arguments
-     */
+public class KartyaJatek {
+    static String[] pakli = new String[22];
+    static String[] ertekek = new String[6];
+    static String[] szinek = new String[4];
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Osszeallit();
+        Kirak();
+         
+         
+         
+        
+    }
+    private static void Osszeallit(){
+        int index = 0;
+        
+        String[] szinek = {"P","T","Z","M"};
+        String[] ertekek = {"Ász","Kir","Fel","X","IX","VIII"};
+        
+        
+        for (int i = 0; i < szinek.length; i++) {
+             for (int j = 0; j < ertekek.length; j++) {
+                 pakli[index] = szinek[i] + " " + ertekek[j];
+                 i++;
+             }
+        }
+        }
+    
+    private static void Kirak(){
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 10; i++) {
+            System.out.println(pakli[i]);
+        }
+        }
+        
     }
     
 }
