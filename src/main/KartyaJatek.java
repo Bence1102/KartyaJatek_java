76de37bac80bc34f4b4f966753cc1d06276c6c1a
S4mur419020 <muraitomi04@gmail.com>
2025-09-11 13:53:39 +0200

    switch case

--- src/main/KartyaJatek.java
+++ src/main/KartyaJatek.java
@@ -64,7 +64,7 @@ public class KartyaJatek {
         do {
             System.out.print("melyik oszlop (1-3): ");
             oszlop = sc.nextInt();
-            jo = oszlop <= 1 && oszlop <= 3;
+            jo = oszlop >= 1 && oszlop <= 3;
         } while (!jo);
         return oszlop;
         
@@ -73,7 +73,22 @@ public class KartyaJatek {
     
     
     private static void kever(){
-
+      int kever=3;
+      switch (kever){
+          case 1: 
+              for (int i = 1; i < 7; i++) {
+                 
+              }
+          case 2: 
+              for (int i = 1; i < 7; i++) {
+                 
+              }
+          case 3: 
+              for (int i = 1; i < 7; i++) {
+                 
+              }
+      }
+      
 }
 
 
